af59ff458b0f803f033ca5c61e28b1eab04d00f4
Soumitra <projwebinfo@gmail.com>
2023-02-08 13:14:26 +0530

    Create HomeController.php

---
+++ codeigniter-front-back/app/Controllers/Back/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers\Back;
+
+use App\Controllers\BaseController;
+
+class HomeController extends BaseController {
+    
+    public function index() {
+        die('Admin Page');
+    }
+}
